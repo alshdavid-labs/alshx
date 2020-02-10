3f2a0488058b8ee5a68196cca73915f4330b2e9d
David Alsh <12656294+alshdavid@users.noreply.github.com>
2020-02-10 16:41:03 +1100

    Update commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,49 +1,66 @@
-NO_MESSAGE=0
-AMEND=0
-PUSH=1
+WANTS_MESSAGE=0
 MESSAGE="Update"
+
+WANTS_BRANCH=0
+BRANCH_NAME=""
+
+WANTS_AMEND=0
+
+WANTS_PUSH=1
 for var in "$@"
 do
-    if [ "$MESSAGE" = "<obtaining>" ]; then
+    if [ $WANTS_MESSAGE = 1 ]; then
         MESSAGE=$var
         continue
     fi
+    if [ $WANTS_BRANCH = 1 ]; then
+        BRANCH_NAME=$var
+        continue
+    fi
+    if [ "$var" = "-b" ]; then
+        WANTS_BRANCH=1
+        continue
+    fi
     if [ "$var" = "-np" ]; then
-        PUSH=0
+        WANTS_PUSH=0
         continue
     fi
     if [ "$var" = "-a" ]; then
-        AMEND=1
+        WANTS_AMEND=1
         continue
     fi
     if [ "$var" = "-ne" ]; then
-        NO_MESSAGE=1
+        WANTS_MESSAGE=1
         continue
     fi
     if [ "$var" = "-m" ]; then
-        MESSAGE="<obtaining>"
+        WANTS_MESSAGE=1
         continue
     fi
 done
 
+if [ $WANTS_BRANCH = 1 ]; then
+    git checkout -b $BRANCH_NAME
+fi
+
 git add .
 
-if [ $AMEND = 1 ]; then
-    if [ $NO_MESSAGE = 1 ]; then
-        git commit --amend --no-edit
+if [ $WANTS_AMEND = 1 ]; then
+    if [ $WANTS_MESSAGE = 1 ]; then
+        git commit --amend -m $MESSAGE
     else
-        git commit --amend -m \"$MESSAGE\"
+        git commit --amend --no-edit
     fi
 else
-    git commit -m \"$MESSAGE\"
+    git commit -m $MESSAGE
 fi
 
 if [ $? != 0 ]; then
     return
 fi
 
-if [ $PUSH = 1 ]; then
-    if [ $AMEND = 1 ]; then
+if [ $WANTS_PUSH = 1 ]; then
+    if [ $WANTS_AMEND = 1 ]; then
         git push -u origin HEAD -f
     else
         git push -u origin HEAD
