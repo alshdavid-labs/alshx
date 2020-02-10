0d99ca15c498fca666e3e3d7c5654d3b65bdf2fb
David Alsh <12656294+alshdavid@users.noreply.github.com>
2020-02-10 16:52:50 +1100

    Update commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,52 +1,59 @@
-WANTS_MESSAGE=0
+readonly STAGE_UNSET=0
+readonly STAGE_PENDING=1
+readonly STAGE_SET=2
+
+WANTS_MESSAGE=$STAGE_UNSET
 MESSAGE="Update"
 
-WANTS_BRANCH=0
+WANTS_BRANCH=$STAGE_UNSET
 BRANCH_NAME=""
 
-WANTS_AMEND=0
+WANTS_AMEND=$STAGE_UNSET
 
-WANTS_PUSH=1
+WANTS_PUSH=$STAGE_SET
 for var in "$@"
 do
-    if [ $WANTS_MESSAGE = 1 ]; then
+    if [ $WANTS_MESSAGE = $STAGE_PENDING ]; then
+        WANTS_MESSAGE=$STAGE_SET
         MESSAGE=$var
         continue
     fi
-    if [ $WANTS_BRANCH = 1 ]; then
+    if [ $WANTS_BRANCH = $STAGE_PENDING ]; then
+        WANTS_BRANCH=$STAGE_SET
         BRANCH_NAME=$var
         continue
     fi
-    if [ "$var" = "-b" ]; then
-        WANTS_BRANCH=1
-        continue
-    fi
-    if [ "$var" = "-np" ]; then
-        WANTS_PUSH=0
-        continue
-    fi
-    if [ "$var" = "-a" ]; then
-        WANTS_AMEND=1
-        continue
-    fi
     if [ "$var" = "-ne" ]; then
-        WANTS_MESSAGE=1
+        WANTS_MESSAGE=$STAGE_SET
         continue
     fi
     if [ "$var" = "-m" ]; then
-        WANTS_MESSAGE=1
+        WANTS_MESSAGE=$STAGE_PENDING
+        continue
+    fi        
+    if [ "$var" = "-b" ]; then
+        WANTS_BRANCH=$STAGE_PENDING
         continue
     fi
+    if [ "$var" = "-np" ]; then
+        WANTS_PUSH=$STAGE_UNSET
+        continue
+    fi
+    if [ "$var" = "-a" ]; then
+        WANTS_AMEND=$STAGE_SET
+        continue
+    fi
+    
 done
 
-if [ $WANTS_BRANCH = 1 ]; then
+if [ $WANTS_BRANCH = $STAGE_SET ]; then
     git checkout -b $BRANCH_NAME
 fi
 
 git add .
 
-if [ $WANTS_AMEND = 1 ]; then
-    if [ $WANTS_MESSAGE = 1 ]; then
+if [ $WANTS_AMEND = $STAGE_SET ]; then
+    if [ $WANTS_MESSAGE = $STAGE_SET ]; then
         git commit --amend -m $MESSAGE
     else
         git commit --amend --no-edit
@@ -59,8 +66,8 @@ if [ $? != 0 ]; then
     return
 fi
 
-if [ $WANTS_PUSH = 1 ]; then
-    if [ $WANTS_AMEND = 1 ]; then
+if [ $WANTS_PUSH = $STAGE_SET ]; then
+    if [ $WANTS_AMEND = $STAGE_SET ]; then
         git push -u origin HEAD -f
     else
         git push -u origin HEAD
