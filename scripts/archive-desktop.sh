bb078f5db144ab95c7c9779c060ce70abd79485d
David Alsh <12656294+alshdavid@users.noreply.github.com>
2020-10-23 19:47:03 +1100

    Update archive-desktop.sh

--- scripts/archive-desktop.sh
+++ scripts/archive-desktop.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-if [ "$(ls $HOME/Desktop)" = "" ]; then
-  echo "Nothing to copy"
-  exit 0
+if [ "$ARCHIVE_DESKTOP_SRC" != "" ] && [ -d "$ARCHIVE_DESKTOP_SRC" ]; then
+  DESKTOP="$ARCHIVE_DESKTOP_SRC"
+elif [ -d "$HOME/Desktop" ]; then
+  DESKTOP="$HOME/Desktop"
+else
+  echo "---------------"
+  echo "Archive Desktop"
+  echo "---------------"
+  echo "Move your desktop to a folder by date"
+  echo
+  echo "Useage: archive-desktop [dest]"
+  echo "eg: archive-desktop ~/Archive"
+  echo
+  echo  "ERROR"
+  echo  "Desktop directory not found"
+  echo
+  echo "You can set a variables:"
+  echo "ARCHIVE_DESKTOP_DEST=\"\$HOME/Archive\"" 
+  echo "ARCHIVE_DESKTOP_SRC=\"/mnt/c/Users/alshd/Desktop\""
+  exit 1
 fi
 
-DESKTOP=$HOME/Desktop
-
-if [ "$ARCHIVE_DESKTOP_SRC" != "" ]; then
-  DESKTOP=$ARCHIVE_DESKTOP_SRC
+if [ "$(ls $DESKTOP)" = "" ]; then
+  echo "Nothing to copy"
+  exit 0
 fi
 
 if [ "$ARCHIVE_DESKTOP_DEST" != "" ]; then
@@ -20,17 +36,21 @@ if [ "$1" != "" ]; then
 fi
 
 if [ -z "$DEST" ]; then
-    echo "---------------"
-    echo "Archive Desktop"
-    echo "---------------"
-    echo "Move your desktop to a folder by date"
-    echo
-    echo "Useage: archive-desktop [dest]"
-    echo "eg: archive-desktop ~/Archive"
-    echo
-    echo "You can also set a variable:"
-    echo "ARCHIVE_DESKTOP_DEST=\"\$HOME/Archive\"" 
-    exit 1
+  echo "---------------"
+  echo "Archive Desktop"
+  echo "---------------"
+  echo "Move your desktop to a folder by date"
+  echo
+  echo "Useage: archive-desktop [dest]"
+  echo "eg: archive-desktop ~/Archive"
+  echo
+  echo  "ERROR"
+  echo  "Output directory not found"
+  echo
+  echo "You can set a variables:"
+  echo "ARCHIVE_DESKTOP_DEST=\"\$HOME/Archive\"" 
+  echo "ARCHIVE_DESKTOP_SRC=\"/mnt/c/Users/alshd/Desktop\""
+  exit 1
 fi
 
 DAY=`date +%d`
