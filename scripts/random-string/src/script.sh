d981c4a8b013475cdd45252539ff5ad90bd61251
David Alsh <alshdavid@gmail.com>
2021-04-01 04:42:24 -0700

    random-package

--- scripts/random-string/src/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-LENGTH=$1
-
-if [ -z $1 ]; then
-    LENGTH=10
-fi
-
-openssl rand -base64 $LENGTH
--- scripts/random/src/index.js
+++ scripts/random/src/index.js
@@ -15,3 +15,10 @@ if (arg === 'string') {
   const id = crypto.randomBytes(length).toString("hex");
   console.log(id);
 }
+
+if (arg === 'uuid' || arg === 'guid') {
+  const guid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.randomInt(1000) & 15 >> c / 4).toString(16)
+  );
+  console.log(guid)
+}
