6129c5db47546a0547c0e2e1088bbb26192b54ba
David Alsh <12656294+alshdavid@users.noreply.github.com>
2022-10-30 17:22:56 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+echo "Alshx Linux and MacOS Installer"
+echo 
+
 LINK="https://github.com/alshdavid/alshx/releases/latest/download"
 FILENAME="NULL"
-OUTDIR="/usr/local/alshx"
+OUT_DIR="/usr/local/alshx"
+TEMP_DIR="$TMPDIR"
 
 OS_TYPE="$(uname -s)"
 CPU_TYPE="$(uname -m)"
@@ -35,22 +39,24 @@ if [ "$OS_TYPE" = Darwin ] && [ "$CPU_TYPE" = "arm64" ]; then
   FILENAME="alshx-darwin-arm64.tar.gz"
 fi
 
-if [ "$FILENAME" = "" ]; then
+if [ "$FILENAME" = "NULL" ]; then
   echo "Unable to identify computer type"
   exit 1
 fi
 
-if [ -d "$OUTDIR" ]; then
-  echo "Tools Detected, Upgrading"
-  sudo rm -rf "$OUTDIR"
+if [ "$TEMP_DIR" = "" ] && [ -d "/tmp" ]; then
+  TEMP_DIR="/tmp/"
+else
+  echo "Unable to find temp directory"
+  exit 1
 fi
 
-sudo mkdir "$OUTDIR"
-
+echo "Temp directory: \"$TEMP_DIR\""
+echo "Downloading archive: \"$FILENAME\""
 if [ -x "$(command -v curl)" ]; then
-  curl -s -o "$TMPDIR/$FILENAME"  "$LINK/$FILENAME"
+  sudo curl -L -s -o "$TEMP_DIR$FILENAME"  "$LINK/$FILENAME"
 elif [ -x "$(command -v wget)" ]; then
-  wget --quiet -O "$TMPDIR/$FILENAME" "$LINK/$FILENAME"
+  sudo wget --quiet -O "$TEMP_DIR$FILENAME" "$LINK/$FILENAME"
 else
   echo "Need either Curl or Wget"
   exit 1
@@ -61,15 +67,28 @@ if ! [ "$?" = "0" ]; then
   exit 1
 fi
 
-sudo tar -C "$OUTDIR" -xzf "$TMPDIR/$FILENAME"
+if [ -d "$OUT_DIR" ]; then
+  echo "Alshx already installed"
+  echo "Removing directory: \"$OUT_DIR\""
+  sudo rm -rf "$OUT_DIR"
+fi
+
+echo "Creating directory: \"$OUT_DIR\""
+sudo mkdir "$OUT_DIR"
+
+echo "Unpacking archive \"$FILENAME\""
+sudo tar -C "$OUT_DIR" -xzf "$TEMP_DIR$FILENAME"
 if ! [ "$?" = "0" ]; then
   echo Failed to extract file
   exit 1
 fi
 
-rm -r -f "$TMPDIR/$FILENAME"
+echo "Removing archive \"$TEMP_DIR$FILENAME\""
+sudo rm -r -f "$TEMP_DIR$FILENAME"
 
 export PATH=/usr/local/alshx:$PATH
 
-echo "add this to your profile: (/etc/profile or /etc/zprofile)"
-echo "export PATH=$OUTDIR:\$PATH"
+echo
+echo -e "Add this to your profile: (/etc/profile or /etc/zprofile)"
+echo -e "    export PATH=$OUT_DIR:\$PATH"
+echo
